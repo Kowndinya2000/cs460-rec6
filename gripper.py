f04500a803976065928e4a1981f5946711328e50
Kowndinya Boyalakuntla <kb1204@scarletmail.rutgers.edu>
2025-10-08 22:46:43 -0400

    Update gripper.py

--- gripper.py
+++ gripper.py
@@ -1,6 +1,6 @@
 import numpy as np
+import time 
 import pybullet
-import time
 
 class Robotiq2F85:
   """Gripper handling for Robotiq 2F85."""
@@ -9,107 +9,79 @@ class Robotiq2F85:
     self._pb = bulletClient
     self.robot = robot
     self.tool = tool
-    
-    # TODO: Set an initial position (x, y, z) for the gripper base.
-    
-    # TODO: Convert desired orientation from Euler angles to quaternion.
-    
-    # TODO: Load the Robotiq 2F-85 URDF into simulation.
-   
-    # TODO: Get the number of joints in the gripper model.
-   
-    # TODO: Create a fixed joint constraint between robot tool and gripper base.
-   
-    # TODO: Set dynamics (friction coefficients) for all gripper fingers.
-   
-    # TODO: Define which joint is the motor joint (controls open/close).
-   
+    pos = [0.1339999999999999, -0.49199999999872496, 0.5]
+    rot = self._pb.getQuaternionFromEuler([np.pi, 0, np.pi])
+    urdf = "robotiq_2f_85/robotiq_2f_85.urdf"
+    self.body = self._pb.loadURDF(urdf, pos, rot)
+    self.n_joints = self._pb.getNumJoints(self.body)
+    self.activated = False
+    print("Num Joints in the Gripper:", self.n_joints)
+    # Connect gripper base to robot tool.
+    self._pb.createConstraint(self.robot, tool, self.body, 0, jointType=pybullet.JOINT_FIXED, jointAxis=[0, 0, 0], parentFramePosition=[0, 0, 0], childFramePosition=[0, 0, -0.07], childFrameOrientation=pybullet.getQuaternionFromEuler([0, 0, np.pi / 2]))
+
+    # Set friction coefficients for gripper fingers.
+    for i in range(self._pb.getNumJoints(self.body)):
+      self._pb.changeDynamics(self.body, i, lateralFriction=10.0, spinningFriction=1.0, rollingFriction=1.0, frictionAnchor=True)
+
+    self.motor_joint = 1
+
+  # Control joint positions by enforcing hard contraints on gripper behavior.
+  # Set one joint as the open/close motor joint (other joints should mimic).
   def update(self):
-    """Update gripper finger joint positions to mimic motor joint."""
-    # TODO: Read current joint states.
-   
-    # TODO: Define which joints should mimic the motor joint.
-   
-    # TODO: Define target joint values as a function of the motor joint.
-   
-    # TODO: Send position control commands to mimic joints.
-   
+    currj = [self._pb.getJointState(self.body, i)[0] for i in range(self.n_joints)]
+    indj = [6, 3, 8, 5, 10]
+    targj = [currj[1], -currj[1], -currj[1], currj[1], currj[1]]
+    self._pb.setJointMotorControlArray(self.body, indj, pybullet.POSITION_CONTROL, targj, positionGains=np.ones(5))
+
+  # Close gripper fingers.
   def activate(self):
-    """Close gripper fingers."""
-    # Apply velocity control to close motor joint.
-    self._pb.setJointMotorControl2(
-      self.body, self.motor_joint,
-      pybullet.VELOCITY_CONTROL,
-      targetVelocity=1, force=10
-    )
+    self._pb.setJointMotorControl2(self.body, self.motor_joint, pybullet.VELOCITY_CONTROL, targetVelocity=1, force=10)
     self.activated = True
 
+  # Open gripper fingers.
   def release(self):
-    """Open gripper fingers."""
-    # Apply velocity control to open motor joint.
-    self._pb.setJointMotorControl2(
-      self.body, self.motor_joint,
-      pybullet.VELOCITY_CONTROL,
-      targetVelocity=-1, force=10
-    )
+    self._pb.setJointMotorControl2(self.body, self.motor_joint, pybullet.VELOCITY_CONTROL, targetVelocity=-1, force=10)
     self.activated = False
 
+  # If activated and object in gripper: check object contact.
+  # If activated and nothing in gripper: check gripper contact.
+  # If released: check proximity to surface (disabled).
   def detect_contact(self):
-    """
-    Detect contact depending on activation state:
-      - If activated & grasping something: check object contact.
-      - If activated & empty: check gripper contact.
-      - If released: TODO (currently disabled).
-    """
-    # TODO: Use raycast proximity to detect possible contact.
     obj, _, ray_frac = self.check_proximity()
-
     if self.activated:
-      # TODO: Check if fingers are nearly closed (empty grasp).
-      
-      # TODO: Ensure we are not detecting collisions with self or world (id=0).
-      
-      # TODO: Return whether external contacts exist.
-        pass    
-    pass
-  
+      empty = self.grasp_width() < 0.01
+      cbody = self.body if empty else obj
+      if obj == self.body or obj == 0:
+        return False
+      return self.external_contact(cbody)
+  #   else:
+  #     return ray_frac < 0.14 or self.external_contact()
+
+  # Return if body is in contact with something other than gripper
   def external_contact(self, body=None):
-    """Check if given body is in contact with something external."""
     if body is None:
       body = self.body
-    # Get all contact points involving this body.
     pts = self._pb.getContactPoints(bodyA=body)
-    # Filter out self-collisions.
     pts = [pt for pt in pts if pt[2] != self.body]
-    return len(pts) > 0
+    return len(pts) > 0  # pylint: disable=g-explicit-length-test
 
   def check_grasp(self):
-    """Wait until gripper stops moving, then check if grasp succeeded."""
-    #Wait until gripper finishes moving.
     while self.moving():
       time.sleep(0.001)
-    # TODO: Decide success if grasp width is above threshold.
-    pass 
+    success = self.grasp_width() > 0.01
+    return success
+
   def grasp_width(self):
-    """Compute distance between left and right fingertips."""
-    # TODO: Get world positions of left & right finger pads.
-    
-    # TODO: Compute Euclidean distance minus offset.
-    pass
+    lpad = np.array(self._pb.getLinkState(self.body, 4)[0])
+    rpad = np.array(self._pb.getLinkState(self.body, 9)[0])
+    dist = np.linalg.norm(lpad - rpad) - 0.047813
+    return dist
 
   def check_proximity(self):
-    """Raycast from end-effector to tool to detect nearby objects."""
-    # Get end-effector and tool positions.
     ee_pos = np.array(self._pb.getLinkState(self.robot, self.tool)[0])
     tool_pos = np.array(self._pb.getLinkState(self.body, 0)[0])
-
-    # Normalize direction vector.
     vec = (tool_pos - ee_pos) / np.linalg.norm((tool_pos - ee_pos))
-
-    # Create ray endpoint slightly ahead of EE.
     ee_targ = ee_pos + vec
-
-    # Cast ray and unpack result.
     ray_data = self._pb.rayTest(ee_pos, ee_targ)[0]
     obj, link, ray_frac = ray_data[0], ray_data[1], ray_data[2]
     return obj, link, ray_frac
