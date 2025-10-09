b34fd85b3b37a0da574084e17d79ae508f37bdca
Kowndinya Boyalakuntla <kb1204@scarletmail.rutgers.edu>
2025-10-08 22:46:15 -0400

    Update robot.py with Pick-Place Env Construction

--- robot.py
+++ robot.py
@@ -21,7 +21,7 @@ from variables import PICK_TARGETS, COLORS, PLACE_TARGETS, BOUNDS, PIXEL_SIZE
 class PickPlaceEnv():
 
   def __init__(self):
-    self.dt = 1/240
+    self.dt = 1/240 # Simulation Physics Frequency (240 Hz) ~ 4.17 ms
     self.sim_step = 0
 
     # Initialize PyBullet client in GUI mode
@@ -30,27 +30,43 @@ class PickPlaceEnv():
 
     # Add search paths for assets
     # TODO: Add pybullet_data and local assets to search path
+    assets_path = os.getcwd() # os.path.dirname(os.path.abspath(""))
+    self._pb.setAdditionalSearchPath(assets_path)
+    self._pb.setAdditionalSearchPath(pybullet_data.getDataPath())
 
     # Set simulation timestep
     # TODO: set time step using self._pb.setTimeStep()
+    self._pb.setTimeStep(self.dt)
 
     # Configure camera view (angle, zoom, target position)
     # TODO: use resetDebugVisualizerCamera()
-
+    self._pb.resetDebugVisualizerCamera(
+      cameraDistance = 0.8,
+      cameraYaw=45,
+      cameraPitch=-40,
+      cameraTargetPosition=[0, -0.5, 0]
+    )
     # Robot Home Configuration
     # TODO: home joint pos, link IDs of the robot and End Effector (EE).
-
+    self.home_joints = [np.pi/2, -np.pi/2, np.pi/2, -np.pi/2, 3 * np.pi/2, 0] # J0, J1, J2, J3, J4, J5
+    self.ee_link_id = 9 # Link ID
+    self.tip_link_id = 10 # Link ID of the gripper finger tips 
+    self.home_ee_euler = [np.pi, 0, np.pi] # RX,RY,RZ for the gripper/EEF orientation
 
   def reset(self, config):
     self._pb.resetSimulation(pybullet.RESET_USE_DEFORMABLE_WORLD)
     self._pb.setGravity(0, 0, -9.8)
     self.cache_video = []
-  
+
     # Load Plane
     # TODO: Your Code here
+    self._pb.loadURDF("plane.urdf", [0, 0, -0.0005])
 
     # Load UR5e arm
     # TODO: Your Code here
+    self.robot_id = self._pb.loadURDF("ur5e/ur5e.urdf", [0, 0, 0],
+                                      flags=pybullet.URDF_USE_MATERIAL_COLORS_FROM_MTL)
+    
 
     self.joint_ids = [self._pb.getJointInfo(self.robot_id, i) for i in range(self._pb.getNumJoints(self.robot_id))]
     self.joint_ids = [j[0] for j in self.joint_ids if j[2] == pybullet.JOINT_REVOLUTE]
@@ -58,6 +74,7 @@ class PickPlaceEnv():
     # Move robot to home configuration.
     for i in range(len(self.joint_ids)):
       # TODO: Your code here
+      self._pb.resetJointState(self.robot_id, self.joint_ids[i], self.home_joints[i])
       pass
 
     # Add gripper.
@@ -110,16 +127,32 @@ class PickPlaceEnv():
       self._pb.stepSimulation()
 
     # return object positions and observations
-    return obj_xyz, self.get_observation()
+    return obj_xyz
+  # , self.get_observation()
 
   def servoj(self, joints):
     """Move to target joint positions with position control."""
     # TODO: Your code here
+    self._pb.setJointMotorControlArray(
+      bodyIndex=self.robot_id,
+      jointIndices=self.joint_ids,
+      controlMode=pybullet.POSITION_CONTROL,
+      target_position=joints,
+      positionGains=[0.01]*6
+    )
     pass
   
   def movep(self, position):
     """Move to target end effector position."""
     # TODO: Your code here
+    joints = self._pb.calculateInverseKinematics(
+      bodyUniqueId=self.robot_id,
+      endEffectorLinkIndex=self.tip_link_id,
+      target_position=position,
+      target_orientation=self._pb.getQuaternionfromEuler(self.home_ee_euler),
+      maxNumIterations=100
+    )
+    self.servoj(joints=joints)
     pass
 
   def step(self, action=None):
@@ -132,18 +165,53 @@ class PickPlaceEnv():
     place_xyz[2] = 0.15
 
     # TODO: Move to hover above pick 
+    ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
+    while np.linalg.norm(hover_xyz - ee_xyz) > 0.01:
+      self.movep(hover_xyz)
+      self.step_sim_and_render()
+      ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
 
     # TODO: Descend to pick
-
+    while np.linalg.norm(pick_xyz - ee_xyz) > 0.01:
+      self.movep(pick_xyz)
+      self.step_sim_and_render()
+      ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
+      
 
     # TODO: Close gripper
+    self.gripper.activate()
+    for _ in range(240):
+      self.step_sim_and_render()
 
     # TODO: Lift back up
-
+    while np.linalg.norm(hover_xyz - ee_xyz) > 0.01:
+      self.movep(hover_xyz)
+      self.step_sim_and_render()
+      ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
+    
     # TODO: Move to place location
-
+    while np.linalg.norm(place_xyz - ee_xyz) > 0.01:
+      self.movep(place_xyz)
+      self.step_sim_and_render()
+      ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
+    
     # TODO: Open gripper (release object)
+    while (not self.gripper.detect_contact()) and (place_xyz[3] > 0.03):
+      place_xyz[2] -= 0.001
+      self.movep(place_xyz)
+      for _ in range(3):
+        self.step_sim_and_render()
 
+    self.gripper.release() 
+    for _ in range(240):
+      self.step_sim_and_render()
+
+    place_xyz[2] = 0.2 
+    while np.linalg.norm(place_xyz - ee_xyz) > 0.01:
+      self.movep(place_xyz)
+      self.step_sim_and_render()
+      ee_xyz = np.float32(self._pb.getLinkState(self.robot_id, self.tip_link_id)[0])
+      
   def step_sim_and_render(self):
     self.gripper.update()
     self._pb.stepSimulation()
