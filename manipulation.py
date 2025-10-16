346b4a6f6346e5bd0b9639f391a01fa5f6e2b6e4
Kowndinya Boyalakuntla <kb1204@scarletmail.rutgers.edu>
2025-10-15 23:47:59 -0400

    update manipulation.py: object localization, and execute pick and place actions.
    Added functionality to capture and process camera image, perform object localization, and execute pick and place actions.

--- manipulation.py
+++ manipulation.py
@@ -11,5 +11,40 @@ time.sleep(1)
 config = {'pick':  ['yellow block', 'green block', 'blue block'],
           'place': ['yellow bowl', 'green bowl', 'blue bowl']}
 env.reset(config)
-time.sleep(1000)
 
+top_view_rgb_img  = env.get_camera_image_top()
+top_view_rgb_img = cv2.cvtColor(top_view_rgb_img, cv2.COLOR_RGB2BGR)
+top_view_rgb_img = np.flipud(top_view_rgb_img.transpose(1, 0, 2))
+top_view_rgb_img = cv2.pyrUp(
+    top_view_rgb_img, 
+    dstsize=(
+        top_view_rgb_img.shape[1]*2,
+        top_view_rgb_img.shape[0]*2
+    )
+)
+
+cv2.imwrite("top_view.png", top_view_rgb_img)
+
+# Perform localization of the objects
+perception_results = localize_objects(top_view_rgb_img)
+
+# Pick and place 
+actions = {
+    "pick": perception_results['yellow block'],
+    "place" : perception_results['yellow bowl']
+}
+env.step(action=actions)
+
+actions = {
+    "pick": perception_results['blue block'],
+    "place" : perception_results['blue bowl']
+}
+env.step(action=actions)
+
+actions = {
+    "pick": perception_results['green block'],
+    "place" : perception_results['green bowl']
+}
+env.step(action=actions)
+
+time.sleep(5)
